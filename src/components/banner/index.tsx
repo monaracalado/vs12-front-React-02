19ef4d31ba55cdbcd9f1454e401bae4c55ae8aef
Monara Calado <monaracalado@gmail.com>
2023-08-05 22:08:31 -0300

    wip

--- src/components/banner/index.tsx
+++ src/components/banner/index.tsx
@@ -4,16 +4,17 @@ interface BannerPrpos{
     h1: string;
     src: string;
     alt: string;
+    title?: string;
     p: string;
     children?:any;
 }
 
-export default function Banner({h1, src, alt, p,children}: BannerPrpos): JSX.Element {
+export default function Banner({h1, src, alt,title , p,children}: BannerPrpos): JSX.Element {
     return(
         <>
         <div className="divMain">
             <h1 className="titleBanner">{h1}</h1>
-            <img className="imgBanner" src={src} alt={alt} />
+            <img className="imgBanner" src={src} alt={alt} title={title}/>
             <p className="textBanner">{p}</p>
             {children}
         </div>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import Banner from "../../components/banner";
 export default function Home(){
     return(
         <>
-        <Banner h1={"Telescopio espacial James Webb"} src={"telescopio-espacial-james-webb.webp"} alt={"Telescopio espacial James Webb"} p={`O Telescópio Espacial James Webb, em inglês James Webb Space Telescope (JWST), é um telescópio espacial projetado pela Nasa em colaboração com a Agência Espacial Europeia (ESA) e a Agência Espacial Canadiana (CSA) com o intuito de substituir o Telescópio Espacial Hubble. 
+        <Banner h1={"Telescopio espacial James Webb"} src={"telescopio-espacial-james-webb.webp"} alt={"Telescopio espacial James Webb"} title={'Telescopio espacial James Webb'} p={`O Telescópio Espacial James Webb, em inglês James Webb Space Telescope (JWST), é um telescópio espacial projetado pela Nasa em colaboração com a Agência Espacial Europeia (ESA) e a Agência Espacial Canadiana (CSA) com o intuito de substituir o Telescópio Espacial Hubble. 
         Ele foi desenvolvido para responder aos questionamentos sobre a formação do universo, a existência de planetas habitáveis, a evolução de galáxias, estrelas, buracos negros e sistemas planetários e investigar com profundidade os exoplanetas. Ele possui mais de 100 vezes a sensibilidade a ondas eletromagnéticas do Telescópio Espacial Hubble e possui um espelho de captura de imagens 2,5 vezes maiores que o mesmo`}>
             <p className="textBanner">O Telescópio Espacial James Webb está localizado a aproximadamente 1.500.000 quilômetros da Terra, no ponto Sol-Terra L2 Lagrange, orbitando ao redor da Terra e do Sol, estando mais distante que a Lua, que está a 384.400 quilômetros da Terra.
 
